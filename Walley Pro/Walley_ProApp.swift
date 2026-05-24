2ebaf715a93be891cfe2b08a88c1f3d3d4379d57
Karthik C <saikarthik.c@gmail.com>
2026-05-24 19:58:36 +0530

    Initial Commit

---
+++ Walley Pro/Walley_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Walley_ProApp.swift
+//  Walley Pro
+//
+//  Created by Sai Karthik on 24/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Walley_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
